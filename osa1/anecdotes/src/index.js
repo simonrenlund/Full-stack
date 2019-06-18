7cbe6e407cfa1defa5caf25b828b8848bbea5f6a
Simon Renlund <renlund.simon@gmail.com>
2019-06-18 11:20:17 +0300

    part1 finished

--- osa1/anecdotes/src/index.js
+++ osa1/anecdotes/src/index.js
@@ -1,13 +1,47 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({handleClick,text}) => <button onClick={handleClick}>{text}</button>
+
+const Points = ({points}) => {
+  return (
+    <p>has {points} votes</p>
+  )
+}
+
+const Top = ({anecdotes, votes}) => {
+  let top = 0
+  for (let i = 0; i < 6;i++) {
+    if (votes[i] > votes[top]) {
+      top = i
+    }
+  }
+  return(
+    <p>{anecdotes[top]} <br></br>has {votes[top]} votes.</p>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-
+  const [votes, addPoint] = useState([0,0,0,0,0,0])
   return (
     <div>
-      {props.anecdotes[selected]}
-      
+      <h2>Anecdote of the day</h2>
+      <p>{props.anecdotes[selected]}</p>
+      <Points points={votes[selected]} />
+      <Button handleClick={() => {
+        let number = Math.floor((Math.random()*6))
+        return setSelected(number)
+      }} text="next anecdote" />
+      <Button handleClick={() => {
+        const copy = [...votes]
+        copy[selected] += 1
+        addPoint(copy)
+        //console.log(copy[selected])
+      }}text="vote" />
+
+      <h2>Anecdote with most votes</h2>
+      <Top anecdotes={props.anecdotes} votes={votes} />
     </div>
   )
 }
