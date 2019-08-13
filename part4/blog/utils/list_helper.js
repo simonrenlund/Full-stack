824eb67494d28f0c2e58ffadfa8ff442e290a6c3
Simon Renlund <simon.renlund@aalto.fi>
2019-08-13 19:55:35 +0300

    added function favoriteBlog and tests for it. skipping 4.6 and 4.7 for now

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -8,7 +8,27 @@ const totalLikes = (blogs) => {
   }
   return blogs.reduce(reducer,0)
 }
+
+const favoriteBlog = (blogs) => {
+  let hindex = 0
+  for (let i=0;i<blogs.length;i++) {
+    if (blogs[i].likes > blogs[hindex].likes) {
+      hindex = i
+    }
+  }
+  if (blogs[hindex]) {
+    return {
+      title: blogs[hindex].title,
+      author: blogs[hindex].author,
+      likes: blogs[hindex].likes
+    }
+  } else {
+    return {}
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
