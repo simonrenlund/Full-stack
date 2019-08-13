37ac5e841c35782ef411185fa74f182263a7b574
Simon Renlund <simon.renlund@aalto.fi>
2019-08-13 07:32:13 +0300

    added requestlogger and errorhandling middleware (errorhandling NYI into code)

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const Blog = require('./models/blog')
 const config = require('./utils/config')
 const blogRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true }).then( () => {
@@ -15,9 +16,12 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true }).then( () => {
 app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
+app.use(middleware.requestLogger)
 
 
 app.use('/api/blogs', blogRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
