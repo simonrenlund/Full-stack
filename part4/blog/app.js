007e20d4e0a97665ddb4d0181ee256954c313faf
Simon Renlund <simon.renlund@aalto.fi>
2019-08-13 07:20:20 +0300

    Moved the db schema into separate file

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -3,17 +3,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 const config = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true }).then( () => {
