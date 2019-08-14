cd48e027c198126d9b1a4efe8c78f8b1996e2e79
Simon Renlund <simon.renlund@aalto.fi>
2019-08-14 08:23:30 +0300

    added test for backend and a required jest config

---
+++ part4/blog/tests/blog.test.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+describe('backend tests', () => {
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
