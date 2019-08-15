dd708c2f9ac0e37c9e6b98287816009443566525
Simon Renlund <simon.renlund@aalto.fi>
2019-08-15 19:18:39 +0300

    Added DB init to backend tests

--- part4/blog/tests/blog.test.js
+++ part4/blog/tests/blog.test.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await noteObject.save()
+
+  noteObject = new Blog(initialBlogs[1])
+  await noteObject.save()
+})
 
 describe('backend tests', () => {
   test('notes are returned as json', async () => {
