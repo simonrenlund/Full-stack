cb0dae23f0f8d441ea3825fb908da900e3045739
Simon Renlund <simon.renlund@aalto.fi>
2019-09-02 13:47:01 +0300

    Changed DB init to an async operation

--- part4/blog/tests/blog.test.js
+++ part4/blog/tests/blog.test.js
@@ -6,35 +6,33 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7
-  },
-  {
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5
+  },
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7
   }
 ]
-
-beforeEach(async () => {
+beforeEach( async() => {
   await Blog.deleteMany({})
-
-  let blogObject = new Blog(initialBlogs[0])
-  await noteObject.save()
-
-  noteObject = new Blog(initialBlogs[1])
-  await noteObject.save()
+  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
 })
 
 describe('backend tests', () => {
-  test('notes are returned as json', async () => {
-    await api
+  test('blogs are returned as json', async () => {
+    console.log('starting test')
+    const response = await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
+    expect(response.body.length).toBe(initialBlogs.length)
   })
 })
 
