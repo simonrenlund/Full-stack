e88c602936fe99afaf2d48590b2b537289bd1f93
Simon Renlund <simon.renlund@aalto.fi>
2019-09-16 20:55:10 +0300

    Added additional info onClick for blogs

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
+import React,{useState} from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const showWhenVis = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+  <div onClick={toggleVisibility} style={blogStyle}>
     {blog.title} {blog.author}
+    <br style={showWhenVis}></br>
+    <a style={showWhenVis} href={blog.url}>{blog.url}</a>
+    <div style={showWhenVis}>likes: {blog.likes} <button type="submit">like</button></div>
+    <div style={showWhenVis}>added by {blog.user.name}</div>
   </div>
-)
+)}
 
 export default Blog
