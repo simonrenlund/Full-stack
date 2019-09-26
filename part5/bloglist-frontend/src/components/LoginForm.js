ef0336b23de98cc35fcd22c429648bd362baa74c
Simon Renlund <simon.renlund@aalto.fi>
2019-09-26 13:43:03 +0300

    Added PropTypes to LoginForm

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
   username,
@@ -32,5 +33,13 @@ const LoginForm = ({
     </form>
   </div>
 )
+
+LoginForm.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired
+}
 
 export default LoginForm
