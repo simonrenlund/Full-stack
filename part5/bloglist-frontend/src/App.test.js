9487ca4e4f9d342e337f8b13326b8d249f36601d
Simon Renlund <simon.renlund@aalto.fi>
2019-10-04 15:37:46 +0300

    Fixed App test and started a login test

--- part5/bloglist-frontend/src/App.test.js
+++ part5/bloglist-frontend/src/App.test.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import { render,  waitForElement } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 jest.mock('./services/blogs')
 import App from './App'
 
 describe('<App />', () => {
-  test('renders all notes it gets from backend', async () => {
+  test("blogs are not rendered if the user isn't logged in", async() => {
     const component = render(
       <App />
     )
     component.rerender(<App />)
-    await waitForElement(
-      () => component.container.querySelector('.blog')
-    )
 
-    const notes = component.container.querySelectorAll('.blog')
-    expect(notes.length).toBe(4)
+    await waitForElement(
+      () => component.getByText('login')
+    )
+    expect(component.container).not.toHaveTextContent('Blogs')
+  })
+
+  //TODO: Se till att testet funkar ordentligt
+  test('app allows users to log in', async() => {
+    const component = render(
+      <App />
+    )
+    const user = {
+      username: 'tester',
+      token: '1231231214',
+      name: 'Donald Tester'
+    }
+    localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+    component.rerender(<App />)
+    expect(component.container).toHaveTextContent('logout')
   })
 })
