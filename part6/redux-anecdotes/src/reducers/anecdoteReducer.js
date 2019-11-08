6612850b95d96b69f2aa1243071705bafd016728
Simon Renlund <simon.renlund@aalto.fi>
2019-11-08 17:12:17 +0200

    Removed unnecessary functions

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,16 +1,4 @@
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-//const initialState = anecdotesAtStart.map(asObject)
-
 const reducer = (state = [], action) => {
   //console.log('state now: ', state)
   //console.log('action', action)
